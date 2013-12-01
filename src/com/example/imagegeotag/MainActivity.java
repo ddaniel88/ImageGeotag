bfd28859ea8c30c9bb4fea02b69bc425438ec340
manoilov88 <manoilov88@gmail.com>
2013-12-01 19:28:59 +0100

    pokretanje kamere

--- src/com/example/imagegeotag/MainActivity.java
+++ src/com/example/imagegeotag/MainActivity.java
@@ -1,22 +1,88 @@
 package com.example.imagegeotag;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	int TAKE_PHOTO_CODE = 0;
+	public static int count=0;
+	String exportFileLocation = "SnapCam";
 
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public void onCreate(Bundle savedInstanceState)
+	{
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    
+	    try {
+			File exportDir = new File(Environment.getExternalStorageDirectory(), "/" + exportFileLocation);
+			final File phoneDir = new File(Environment.getExternalStorageDirectory(), "/" + exportFileLocation + "/" + android.os.Build.MODEL);
+			if (!exportDir.exists()) {
+				exportDir.mkdirs();
+				if (!phoneDir.exists()) {
+					phoneDir.mkdirs();
+				}
+			} else if (!phoneDir.exists()) {
+				phoneDir.mkdirs();
+			}
+			
+			  Button capture = (Button) findViewById(R.id.btnCapture);
+		        capture.setOnClickListener(new View.OnClickListener() {
+		        public void onClick(View v) {
+		        	
+		        	//1.jpg, 2.jpg i td..
+		            count++;
+		            String file = count+".jpg";
+		            File newfile = new File(phoneDir, file);
+		            try {
+		                newfile.createNewFile();
+		            } catch (IOException e) {
+		            	
+		            }       
+		
+		            Uri outputFileUri = Uri.fromFile(newfile);
+		
+		            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
+		            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+		
+		            startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+
+		        }
+		    });
+		        
+		} catch (NullPointerException ex) {
+			Toast.makeText(getApplicationContext(), "Name cannot be empty.", Toast.LENGTH_SHORT).show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+	      
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	
+	    if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+	        Log.d("CameraDemo", "Pic saved");
+	
+	    }
 	}
 
 }
